ed5f1454a47a996340c7b295c08781534f86ff29
Jens Schauder <jschauder@pivotal.io>
2019-11-15 10:26:02 +0100

    test confirming the problem

--- src/test/java/com/example/demo/Datajpa1629ApplicationTests.java
+++ src/test/java/com/example/demo/Datajpa1629ApplicationTests.java
@@ -2,34 +2,25 @@ package com.example.demo;
 
 import com.example.demo.entity.MyEntity;
 import com.example.demo.repository.MyEntityRepository;
-import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
 class Datajpa1629ApplicationTests {
 
 	@Autowired
 	MyEntityRepository repo;
 
-//	@Autowired
-//	EntityManager em;
-
 	@Test
 	void contextLoads() {
 
-		LocalDate newestDateNotWorking = LocalDate.parse("1800-09-29");
+		LocalDate newestDateNotWorking = LocalDate.parse("1880-09-29");
 		LocalDate oldestDateWorking = LocalDate.parse("1893-09-29");
 
 		long difference = ChronoUnit.DAYS.between(newestDateNotWorking, oldestDateWorking);
@@ -67,40 +58,14 @@ class Datajpa1629ApplicationTests {
 	@Test
 	void reproduceWithSpringData() {
 
-		SoftAssertions.assertSoftly(softly -> {
-			check(softly, this::saveAndReload, "1893-04-01", 1L);
-			check(softly, this::saveAndReload, "1893-04-02", 0L);
-		});
+		check("1893-04-01", 1L);
+		check("1893-04-02", 0L);
 	}
 
-//	@Test
-//	@Disabled
-//	void reproduceWithEM() {
-//
-//		checkEm("1893-04-01", 1L);
-//		checkEm("1893-04-02", 0L);
-//	}
-
-	private void check(SoftAssertions softly, Function<LocalDate, LocalDate> saveAndReload, String date, long expectedDifference) {
-
+	private void check(String date, long expectedDifferenc) {
 		LocalDate testDate = LocalDate.parse(date);
-
-		softly.assertThat(ChronoUnit.DAYS.between(saveAndReload.apply(testDate), testDate))
-				.describedAs(testDate + " should show a difference of " + expectedDifference)
-				.isEqualTo(expectedDifference);
+		assertThat(ChronoUnit.DAYS.between( saveAndReload(testDate), testDate)).isEqualTo(expectedDifferenc);
 	}
-
-//	private void checkEm(String date, long expectedDifference) {
-//		LocalDate testDate = LocalDate.parse(date);
-//		assertThat(ChronoUnit.DAYS.between(saveAndReloadEm(testDate), testDate)).isEqualTo(expectedDifference);
-//	}
-
-//	private LocalDate saveAndReloadEm(LocalDate currentDate) {
-//		MyEntity entity = createInstanceForDate(currentDate);
-//		em.persist(entity);
-//		MyEntity saved = em.find(MyEntity.class, entity.getId());
-//		return saved.getLocalDate();
-//	}
 
 
 	private MyEntity createInstanceForDate(LocalDate date) {
